bee90742535d0f53d72f73f766269412f61f7bb8
Helix <hissyu@hotmail.com>
2021-11-15 05:38:35 +0000

    fix: 站外链接无法正确设置 title

--- scripts/filters/post.js
+++ scripts/filters/post.js
@@ -10,15 +10,15 @@ hexo.extend.filter.register('after_post_render', data => {
 
   const url = require('url');
   const siteHost = url.parse(config.url).hostname || config.url;
-  data.content = data.content.replace(/<a[^>]* href="([^"]+)"[^>]*>([^<]*)<\/a>/img, (match, href, html) => {
-    // Exit if the href attribute doesn't exists.
-    if (!href) return match;
+  data.content = data.content.replace(/<a[^>]* ((?:href)|(?:title))="([^"]+)"[^>]* ((?:href)|(?:title))="([^"]+)"[^>]*>([^<]*)<\/a>/img, (match, attr1, str1, attr2, str2, html) => {
+    let href = attr1 === 'href' ? str1 : str2;
+    let title = attr1 === 'title' ? str1 : str2;
 
     // Exit if the url has same host with `config.url`, which means it's an internal link.
     let link = url.parse(href);
     if (!link.protocol || link.hostname === siteHost) return match;
 
-    return `<span class="exturl" data-url="${Buffer.from(href).toString('base64')}">${html}</span>`;
+    return `<span class="exturl" data-url="${Buffer.from(href).toString('base64')}" title="${title}">${html}</span>`;
   });
 
 }, 0);
